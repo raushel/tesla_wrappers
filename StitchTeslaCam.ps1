f56dc1b69eefc60177907be672cf444e9d17caa4
Mario Raushel <iceraven@iceraven.local>
2019-05-17 23:10:00 -0500

    tesla_dashcam logging, 0.1.9 updates (non-pip), cmd line changes

--- StitchTeslaCam.ps1
+++ StitchTeslaCam.ps1
@@ -11,7 +11,7 @@ $Global:ScriptName = $MyInvocation.MyCommand.ToString()
 #pip install tesla_dashcam==0.1.8
 #pip install tesla_dashcam --upgrade
 
-if(!(Test-Connection $hostname -quiet))
+if((Test-Connection $hostname -quiet))
 {
     LogIt -message ("$hostname is offline, starting run") -component "Test-Connection" -type 1 
     $dir = get-childitem -path $path -Recurse -Directory -Force -ErrorAction SilentlyContinue  | Where-Object {$_.Name -ne $outputFolder} | Select-Object Name,FullName
@@ -23,29 +23,33 @@ if(!(Test-Connection $hostname -quiet))
         $foldername = $folder.fullname
         $fold = $folder.name
         LogIt -message ("Starting:  $foldername") -component "tesla_dashcam" -type 1 
-        #write-host "Starting: " $folder.fullname
+
         $output = $path + $outputFolder + '\' + $folder.name
 
         #0.1.8
-        tesla_dashcam $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --timestamp
+        #$result = tesla_dashcam $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --timestamp
         #0.1.9
-        #tesla_dashcam $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --timestamp --delete_source true --complression veryslow
+        $result = .\tesla_dashcam.exe --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --compression veryslow --no-notification $folder.fullname
         
-        #Set the Created/Modified Date based on the filedate rather than copied date
-        $file = Get-Item "$output.mp4"
-        $datetime = [datetime]$folder.name.substring(0,10) + [TimeSpan]$folder.name.substring(11,8).replace('-',':')
+        $result >> Tesla_Dashcam.log
 
-        $file.LastWriteTime = $datetime
-        $file.CreationTime = $datetime
+        LogIt -message ("Tesla_Dashcam.log updated with last run") -component "tesla_dashcam" -type 2
     
-        #0.1.8 only
         if(Test-Path "$output.mp4")
         {
+
+            #Set the Created/Modified Date based on the filedate rather than copied date
+            $file = Get-Item "$output.mp4"
+            $datetime = [datetime]$folder.name.substring(0,10) + [TimeSpan]$folder.name.substring(11,8).replace('-',':')
+
+            $file.LastWriteTime = $datetime
+            $file.CreationTime = $datetime
+
             LogIt -message ("Deleting:  $fold") -component "Remove-Item" -type 1 
             Remove-Item -Recurse -Force $folder.fullname
         }
         else {
-            LogIt -message ("Error:  $fold not deleted, $output.mp4 not created") -component "Remove-Item" -type 3 
+            LogIt -message ("Error: $output.mp4 not created, see results from tesla_dashcam") -component "Test-Path" -type 3 
         }
     }
     LogIt -message ("Completed Run") -component "Complete" -type 1 
