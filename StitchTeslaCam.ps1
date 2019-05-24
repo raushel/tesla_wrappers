853e2be75b66a82139d2a3a549db265410de8c89
raushel <49056263+raushel@users.noreply.github.com>
2019-05-24 01:32:37 -0500

    Add files via upload
    Change to 0.1.9 Tesla_Dashcam
    Tweaks to support new cmdline options / changes to existing parameters

--- StitchTeslaCam.ps1
+++ StitchTeslaCam.ps1
@@ -7,8 +7,8 @@ $ErrorActionPreference = "Stop"
 $Global:LogFile = $PSScriptRoot.ToString() + '\' + ($MyInvocation.MyCommand.Name).Replace('.ps1','.log')
 $Global:ScriptName = $MyInvocation.MyCommand.ToString()
 
-C:\Python37\python.exe -m pip install --upgrade pip
-pip install tesla_dashcam==0.1.8
+#C:\Python37\python.exe -m pip install --upgrade pip
+#pip install tesla_dashcam==0.1.8
 #pip install tesla_dashcam --upgrade
 Install-PackageProvider NuGet -Force
 Install-Module posh-ssh -force
@@ -40,17 +40,20 @@ if(!$online -or $cam.count -eq 2)
         $output = $path + $outputFolder + '\' + $folder.name
 
         #0.1.8
-        $result = tesla_dashcam $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --timestamp
+        #$result = tesla_dashcam $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --timestamp
         #0.1.9
-        #$result = .\tesla_dashcam.exe $folder.fullname --quality HIGH --layout WIDESCREEN --encoding x265 --output $output --compression veryslow --no-notification
+        $result = .\tesla_dashcam.exe $folder.fullname --quality HIGH --layout WIDESCREEN --mirror --encoding x265 --no-notification --font /Windows/Fonts/arial.ttf
+        
+        #0.1.9 changed --output to also store the temp files in target directory, moving it after completion to avoid Plex issues
+        $path = "$foldername" + "\" + "$fold.mp4"
+        LogIt -message ("Moving $path to $output") -component "Move-Item" -type 2
+        $move = Move-Item -Path $path -Destination "$output.mp4"
         
         $result >> Tesla_Dashcam.log
-
         LogIt -message ("Tesla_Dashcam.log updated with last run") -component "tesla_dashcam" -type 2
     
         if(Test-Path "$output.mp4")
         {
-
             #Set the Created/Modified Date based on the filedate rather than copied date
             $file = Get-Item "$output.mp4"
             $datetime = [datetime]$folder.name.substring(0,10) + [TimeSpan]$folder.name.substring(11,8).replace('-',':')
